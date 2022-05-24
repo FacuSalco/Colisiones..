807673ad6e3634cd0c041f951c9c099ceaaa856d
FacuSalco <facusalco26@gmail.com>
2022-05-24 15:59:07 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(0, 0.5f, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "DeathWall")
+        {
+            //Destroy(gameObject);
+            transform.position  = new Vector3(0, 0.5f, 0);
+        }
+
+        
+    }
+}
